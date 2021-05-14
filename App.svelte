1ba005b17a07f5b96d9820cdc3cb45cbc865bd68
Ashok Raj <ashokrajkoribilli@gmail.com>
2021-05-14 13:15:43 +0530

    Svelte application

---
+++ App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Navbar from "./Navbar.svelte";
+  import Player from "./Player.svelte";
+  import AddPlayer from "./AddPlayer.svelte";
+
+  let players = [
+    {
+      name: "Ashok Raj",
+      points: 53
+    },
+    {
+      name: "Steve Smith",
+      points: 45
+    },
+    {
+      name: "Kane Williamson",
+      points: 34
+    }
+  ];
+
+  const addPlayer = e => {
+    const newPlayer = e.detail;
+    players = [...players, newPlayer];
+  };
+
+  const removePlayer = e => {
+    players = players.filter(player => player.name !== e.detail);
+  };
+</script>
+
+
+<main>
+  <Navbar />
+  <div class="container">
+    <AddPlayer  on:addplayer={addPlayer}/>
+    {#if players.length === 0}
+      <p>No players to display</p>
+    {:else}
+      {#each players as player}
+        <Player name={player.name} points={player.points} on:removeplayer={removePlayer}/>
+      {/each}
+    {/if}
+  </div>
+</main>
---
+++ Player.svelte
@@ -0,0 +1,37 @@
+0;export
+import { createEventDispatcher } from "svelte";
+const dispatch = createEventDispatcher();
+export
+<bubutton></bubutton> clclass=""btn btn-sm btn-dangerDel
+<button class="btn btn-sm btn-danger">Del</button>
+on:clclick={}onDelete
+
+const ononDelete = () => {}Xdisdispatch()""removePlayer, bname btn-smp
+  const toogleControl = () => (showControl = !showControl)
+
+  const onDelete = () => dispatch("removeplayer", name)
+</script>
+
+<style>
+  h1 {
+    color: #204f6e;
+  }
+  h3 {
+    margin-bottom: 10px;
+  }
+</style>
+
+    <div class="card">
+        <h1>Hello {name}! 
+        <button class="btn btn-sm" on:click={toogleControl}>
+          {#if showControl}-{:else}+{/if}
+        </button>
+        <button class="btn btn-danger btn-sm" on:click={onDelete}>X</button>
+        </h1>
+        <h3>Points : {points}</h3>
+        {#if showControl}
+        <button class="btn" on:click={addPoint}>+1</button>
+        <button class="btn btn-dark" on:click={removePoint}>-1</button>
+        <input type="number" bind:value={points} />
+        {/if}
+    </div>
